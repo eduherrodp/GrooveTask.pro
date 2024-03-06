03d22266f87a71aebfe28cef897d05dc25c3e0f0
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-06 12:50:22 -0600

    Spotify authorization page/PKCE verifier and challenge data

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -1,4 +1,6 @@
-const clientId = ""; 
+import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
+
+const clientId = "client-id";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
@@ -10,66 +12,22 @@ if (!code) {
     populateUI(profile);
 }
 
-export async function redirectToAuthCodeFlow(clientId: string) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
-
-    localStorage.setItem("verifier", verifier);
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("scope", "user-read-private user-read-email");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
-
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(Length: number) {
-    let text = '';
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier: string) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        . replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/-+$/, '');
-}
-
-export async function getAccessToken(clientId: string, code: string): Promise<string> {
-    const verifier = localStorage.getItem("verifier");
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("code_verifier", verifier!);
-
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
+async function fetchProfile(code: string): Promise<UserProfile> {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${code}` }
     });
 
-    const { access_token } = await result.json();
-    return access_token;
+    return await result.json();
 }
 
-async function fetchProfile(token: string): Promise<any> {
-    // TODO: Call Web API
-}
-
-function populateUI(profile: any) {
-    // TODO: Update UI with profile data
+function populateUI(profile: UserProfile) {
+    document.getElementById("displayName")!.innerText = profile.display_name;
+    document.getElementById("avatar")!.setAttribute("src", profile.images[0].url)
+    document.getElementById("id")!.innerText = profile.id;
+    document.getElementById("email")!.innerText = profile.email;
+    document.getElementById("uri")!.innerText = profile.uri;
+    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url")!.innerText = profile.href;
+    document.getElementById("url")!.setAttribute("href", profile.href);
+    document.getElementById("imgUrl")!.innerText = profile.images[0].url;
 }
