5b43ecee6ed548500ddddeb1b7b3ae00ee16e994
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 14:32:22 -0600

    backend - endpoint; frontend - api request

--- backend/google-tasks/src/app.js
+++ backend/google-tasks/src/app.js
@@ -1,6 +1,6 @@
 const { google } = require('googleapis');
-const {cors} = require('cors');
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3000;
