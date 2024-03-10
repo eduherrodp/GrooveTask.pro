cae4fb08ba734e7a88ac42829ac5afd8a18dd0f4
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 13:54:30 -0600

    google-tasks-redirect url

--- backend/google-tasks/src/app.js
+++ backend/google-tasks/src/app.js
@@ -14,10 +14,10 @@ const rl = readline.createInterface({
 const oauth2Client = new google.auth.OAuth2(
   '48778211564-of75cphljno4hqfk96pcb41a3saoss0g.apps.googleusercontent.com',
   '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',
-  'https://api.edhrrz.pro'
+  "https://www.edhrrz.pro/pages/dashboard.html"
 );
 
-// Access scopes for read-only Drive activity.
+// Access scopes for read and write tasks
 const scopes = [
   'https://www.googleapis.com/auth/tasks.readonly',
   'https://www.googleapis.com/auth/tasks'
