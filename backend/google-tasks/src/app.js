51c945b63d8046a3b754af46abc4e2b8d0a09a43
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 14:47:58 -0600

    backend - endpoint; frontend - api request

--- backend/google-tasks/src/app.js
+++ backend/google-tasks/src/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Middleware para permitir CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.edhrrz.pro');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
