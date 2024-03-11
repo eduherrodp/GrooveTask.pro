9b7253ec49c464a99789e0b8917d835b34cf5186
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 18:56:20 -0600

    Rendering tasks - backend

--- backend/firebase/src/routes/userRoutes.js
+++ backend/firebase/src/routes/userRoutes.js
@@ -5,12 +5,13 @@ const { signup, getUserInfo } = require('../controllers/userController');
 const { login } = require('../controllers/userController');
 const { logout } = require('../controllers/userController');
 const { saveData } = require('../controllers/userController');
+const { getTaskLists } = require('../controllers/userController');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/info/:uid', getUserInfo);
 router.post('/save', saveData);
-router.post('/exchangeCode', getTaskLists);
+router.post('/tasklist', getTaskLists);
 
 module.exports = router; 
