5c1e8147ac0b509f04a3540c86a576954cac7422
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-11 14:30:23 -0600

    [1]HTTP Request: all task list - ordered

--- backend/firebase/src/controllers/userController.js
+++ backend/firebase/src/controllers/userController.js
@@ -121,7 +121,7 @@ async function getToken(req, res) {
         title: task.title,
         updated: task.updated,
         selfLink: task.selfLink,
-        status: task.status,
+        status: task.completed,
         due: task.due,
         notes: task.notes
       }));
