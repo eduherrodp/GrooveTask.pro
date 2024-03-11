fc6de58d28fb8e6b2e565ca0e1148a14e4ef9aef
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 20:25:05 -0600

    Rendering tasks - backend

--- frontend/src/js/dashboard.js
+++ frontend/src/js/dashboard.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
                     // Send googleCode to the backend to exchange it for a token in server
                     const response = await fetch('https://api.edhrrz.pro/user/getToken', {
-                        method: 'POST',
+                        method: 'GET',
                         headers: {
                             'Content-Type': 'application/json'
                         },
