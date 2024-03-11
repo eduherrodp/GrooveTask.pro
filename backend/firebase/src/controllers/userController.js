20a8d991cc43da276611793bd71751138a707a6e
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 22:37:38 -0600

    Rendering tasks - backend

--- backend/firebase/src/controllers/userController.js
+++ backend/firebase/src/controllers/userController.js
@@ -89,7 +89,7 @@ async function getUserInfo(req, res) {
 const oAuth2Client = new google.auth.OAuth2(
   '48778211564-of75cphljno4hqfk96pcb41a3saoss0g.apps.googleusercontent.com',
   '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',
-  'https://api.edhrrz.pro'
+  'https://www.edhrrz.pro/pages/dashboard.html'
 );
 
 async function getToken(req, res) {
