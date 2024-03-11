fe395f544a04eb5d3265eef4ad2866b178c89173
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 23:25:00 -0600

    Rendering tasks - backend

--- backend/firebase/src/controllers/userController.js
+++ backend/firebase/src/controllers/userController.js
@@ -101,12 +101,8 @@ async function getToken(req, res) {
     oAuth2Client.setCredentials(tokens);
 
     console.log("Tokens:", tokens);
-
-    // Save tokens in the database inside the user collection
-    const userRef = ref(getDatabase(), `users/${uid}/tokens`);
-    await set(userRef, tokens);
-
-    res.status(200).json({ message: "Token obtained successfully", tokens });
+    // Response tokens
+    res.status(200).json({ tokens });
   } catch (error) {
     console.error("Error getting token:", error.message);
     res.status(500).json({ error: error.message });
