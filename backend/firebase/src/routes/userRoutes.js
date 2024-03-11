d7b3a147c8b44a2af119048ce446a66644da7ba7
Eduardo Hernández <eduher.rodp@gmail.com>
2024-03-10 20:09:15 -0600

    Rendering tasks - backend

--- backend/firebase/src/routes/userRoutes.js
+++ backend/firebase/src/routes/userRoutes.js
@@ -7,6 +7,14 @@ const { logout } = require('../controllers/userController');
 const { saveData } = require('../controllers/userController');
 const { getToken } = require('../controllers/userController');
 
+// Middleware para manejar las solicitudes OPTIONS
+router.options('*', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'https://www.edhrrz.pro');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
+});
+
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/logout', logout);
